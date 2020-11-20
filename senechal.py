1cc29614efb35eb24593074a6ba24ef891efe49c
István Svorcz <walakymas@gmail.com>
2020-11-20 09:32:47 +0100

    search

--- senechal.py
+++ senechal.py
@@ -11,10 +11,6 @@ from  random import randint
 bot = commands.Bot(command_prefix='!', description="Szolgálatára Jóuram avagy Hölgyem...")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('Igen uram?')
-
-@bot.command()
 async def d20(ctx):
     await ctx.send(randint(1,20))
 
@@ -35,14 +31,6 @@ async def sum(ctx, numOne: int, numTwo: int):
     await ctx.send(numOne + numTwo)
 
 @bot.command()
-async def Perin(ctx, task="base", param=""):
-    await pcInfo(ctx, 'perin', task, param)
-
-@bot.command()
-async def perin(ctx, task="base", param=""):
-    await pcInfo(ctx, 'perin', task, param)
-
-@bot.command()
 async def pc(ctx, name="", task="base", param=""):
     await pcInfo(ctx, name, task, param)
 
@@ -57,15 +45,20 @@ async def embedNpc(ctx, pc, task, param):
     await ctx.send(embed=embed)
 
 async def npcInfo(ctx, name="", task="base", param=""):
-    if (name in npcs):
-        pc = npcs[name]
-        await embedNpc(ctx, pc, task, param)
-    elif "all" == name:
-        for pc in npcs.values():
-            await embedNpc(ctx, pc, task, param)
+    print(name+","+task)
+    if "all" == name:
+        for pc in pcs.values():
+            await embedPc(ctx, pc, task, param)
     else:
-        await ctx.send(name +'? Sajnos nem ismerek ilyen lovagot')
-
+        count = 0
+        print(npcs)
+        for pc in npcs.values():
+            print(pc['name'])
+            if name.lower() in pc['name'].lower():
+                count += 1
+                await embedNpc(ctx, pc, task, param)
+        if count == 0:
+            await ctx.send(name +'? Ez a név iesmeretlen számomra. Utána érdeklődjem Jóuram?')
 
 def tr(a):
     return str(a)+'/'+str(20-a)
@@ -95,6 +88,10 @@ async def embedPc(ctx, pc, task, param):
         embed.add_field(name="Move Rate", value=str(round((pc['statistics']['dex']+pc['statistics']['siz'])/10)));
         embed.add_field(name="HP", value=str(round((pc['statistics']['con']+pc['statistics']['siz']))));
         embed.add_field(name="Unconscious", value=str(round((pc['statistics']['con']+pc['statistics']['siz'])/4)));
+    elif ("events".startswith(task.lower())):
+        embed = discord.Embed(title=pc['name'],timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+        for name, value in pc['statistics'].items():
+            embed.add_field(name=name, value=value)
     else:        
         embed = discord.Embed(title=pc['name'], description=pc['description'], timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
         for name, value in pc['main'].items():
@@ -105,15 +102,18 @@ async def embedPc(ctx, pc, task, param):
 
 async def pcInfo(ctx, name="", task="base", param=""):
     print(name+","+task)
-    if (name in pcs):
-        pc = pcs[name]
-        await embedPc(ctx, pc, task, param)
-    elif "all" == name:
+    if "all" == name:
         for pc in pcs.values():
             await embedPc(ctx, pc, task, param)
     else:
-        embed = discord.Embed(title=name, timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-        await ctx.send(name +'? Sajnos nem ismerek ilyen lovagot')
+        count = 0
+        for pc in pcs.values():
+            if name.lower() in pc['name'].lower():
+                count+=1
+                await embedPc(ctx, pc, task, param)
+        if count == 0:
+            embed = discord.Embed(title=name, timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+            await ctx.send(name +'? Sajnos nem ismerek ilyen lovagot')
 
 @bot.command()
 async def info(ctx):
@@ -137,33 +137,48 @@ async def youtube(ctx, *, search):
     # I will put just the first result, you can loop the response to show more results
     await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
 
-# Events
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.idle)
     print('My Ready is Body')
 
+@bot.command()
+async def frissito(ctx):
+    database()
+    await ctx.send("Egykupa bort jóuram?"); 
+
 
 @bot.listen()
 async def on_message(message):
-    if "senechal" in message.content.lower():
+    if ("senechal" in message.content.lower() or "seneschal" in message.content.lower()):
         # in this case don't respond with the word "Tutorial" or you will call the on_message event recursively
-        await message.channel.send('Szólított uram?')
+        await message.channel.send("""Szólított uram?
+
+Egyelőre a következő kérdésekre tudok érdemben válaszolni:
+
+!pc """ +str(list(pcs)).replace("'","")+ """ [base|stats|traits] 
+!pc all [base|stats|traits] 
+!npc { névrészlet } 
+!npc all
+!d20
+!d6 [{num}]""")
         await bot.process_commands(message)
     elif "!20" == message.content:
         await d20(message.channel)
 
-# with open(r'npc.yml') as file:
-#    npc = yaml.load(file, Loader=yaml.FullLoader)
+npcs = {}
+pcs = {}
+def database():
+    global npcs, pcs
+    with open(r'npc.yml') as file:
+        npcs = yaml.load(file, Loader=yaml.FullLoader)
+        print(npcs)
+ 
+    with open(r'pc.yml') as file:
+        pcs = yaml.load(file, Loader=yaml.FullLoader)
+        print(pcs)
 
-with open(r'npc.yml') as file:
-    npcs = yaml.load(file, Loader=yaml.FullLoader)
-    print(npcs)
- 
-with open(r'pc.yml') as file:
-    pcs = yaml.load(file, Loader=yaml.FullLoader)
-    print(pcs)
- 
+database()
 with open(r'config.yml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
     print(config['token'])
