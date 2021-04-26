2a187dccdafb13ff18f44cff2a31cfa895f704b3
István Svorcz <walakymas@gmail.com>
2021-04-26 11:07:38 +0200

    minor fixws

--- web/urls.py
+++ web/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf.urls import url
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 from . import views
 
