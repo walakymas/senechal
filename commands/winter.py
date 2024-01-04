98e4d26fbc23b1f5418de93bb34afc3f2f1b2ff0
isvorcz <istvan.svorcz@gmail.com>
2024-01-04 10:11:16 +0100

    Winter minor  changes

--- commands/winter.py
+++ commands/winter.py
@@ -36,14 +36,14 @@ Ha meg voltak adva ebben az évben ***!mark {skill|trait|passion}*** utasításs
             msg += f"Extra bevétel: {dice(20)} \n"
         elif major == 6:
             msg += f"Stewardship({winter['stewardship']}): {dobas} {t}  {('Gazdag lovag vagy','Nem sikerült kihasználni a remek időt')[siker>=3]} \n"
-        embed.add_field(name=f"{year} tele", value=f"```{msg}```", inline=False)
+        #embed.add_field(name=f"{year} tele", value=f"```{msg}```", inline=False)
         msg = ""
         for h in winter['horses']:
             msg += f"  {h}: {('Egészséges','Megdöglött vagy tönkrement')[dice(20)<3]}\n"
-        embed.add_field(name="Lovak",value=f"```{msg}```", inline=False)
-        i = int(char.id)
+        #embed.add_field(name="Lovak",value=f"```{msg}```", inline=False)
+        charId = int(char.id)
         msg = ""
-        rows = MarksTable().list(dbid=i, year=year)
+        rows = MarksTable().list(dbid=charId, year=year)
         msg += f"Modified   Spec              Dobás   Hatás\n";
         marks = []
         for row in rows:
@@ -69,13 +69,25 @@ Ha meg voltak adva ebben az évben ***!mark {skill|trait|passion}*** utasításs
                 s = ""
                 if 'dbid' in f:
                     npc = Character.get_by_id(f['dbid'])
-                    if npc and 'role' in npc.data and npc.data['role'].lower() == 'follower':
-                        for ng, vg in npc.data['skills'].items():
-                            for n, v in vg.items():
-                                if v < 15 or (v <= 20 and randint(1, 6) == 6) or (v > 20 and randint(1, 20) == 20):
-                                    s += f"{n} `{v}` -> `{v+1}`\n"
+                    rows = MarksTable().list(dbid=f['dbid'], year=year)
+                    marks = []
+                    if 'role' in f and (f['role'] == 'wife' or f['role'] == 'lover'):
+                        birth = 0
+                        con = npc.data['stats']['con']
+                        if 'birth' in npc.data['main']:
+                            birth = npc.data['main']['birth']
+                        s += f"Con: {con}, Birth: {birth} \n"
+
+                    s += f"Modified   Spec              Dobás   Hatás\n";
+                    for row in rows:
+                        #print(f"{row}")
+                        for t, name, value, *name2 in get_checkable(npc.get_data(), row[5]):
+                            if name not in marks:
+                                marks.append(name)
+                                (color, text, ro, success) = check(value, 0)
+                                s += f"{str(row[2])[:10]} {name:15} {ro:2} vs {value:2}  {('---', 'Increase')[ro > value]}\n"
                     if s != "":
-                        embed.add_field(name=f"{npc.data['name']} ({f['connection']})", value=s, inline=False)
+                        embed.add_field(name=f"{npc.data['name']} ({f['connection']})", value=f"```{s}```", inline=False)
                 else:
                     if 'skills' in f:
                         for n, v in f['skills'].items():
